297a5d3690785b20b079f4566a459275797d834f
wayn3r <w9delacruz@gmail.com>
2022-03-13 23:26:36 -0400

    serving statics

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,8 +1,14 @@
 import { Module } from '@nestjs/common';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 import { TaskModule } from './task/module/task.module';
 
 @Module({
-  imports: [TaskModule],
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '../..', 'app/dist/app'),
+    }),
+    TaskModule],
   controllers: [],
   providers: [],
 })
