12e5b60abe8052e6079a39632e595bd4fa2158e5
wayn3r <w9delacruz@gmail.com>
2024-01-25 10:00:54 -0400

    feat(api): use env PORT

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
@@ -8,6 +9,10 @@ async function bootstrap() {
       allowedHeaders: '*',
     },
   });
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
+
+  const logger = new Logger('NestApplication');
+  logger.log(`Application is running on: ${await app.getUrl()}`);
+
 }
 bootstrap();
